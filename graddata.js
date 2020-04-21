db7e988aaba0559245ec12f525d705745ad75dec
Narendra-Rathore <41288836+Narendra-Rathore@users.noreply.github.com>
2020-04-21 22:46:20 +0530

    Add files via upload

---
+++ graddata.js
@@ -0,0 +1,203 @@
+var vgrad = vgrad || {};
+vgrad.GradData = [
+{
+	barcode: 300002,
+	firstname: 'Elizabeth',
+	lastname: 'Beck',
+	audiofile: '2520509.mp3',
+	photofile: 'Beck_Elizabeth.jpg',
+	altText: 'Elizabeth Anne Beck, Civil Engineering',
+	filter1: 'College of Engineering',
+	filter2: 'Master'
+},
+{
+	barcode: 300017,
+	firstname: 'Brandon',
+	lastname: 'Sparks',
+	audiofile: '2520516.mp3',
+	photofile: 'Sparks_Brandon.jpg',
+	altText: 'Brandon Grant Sparks, Biology, "Focus on what you can control". Thanks Mom and Dad for being there for me through all FIVE years.',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Master'
+},
+{
+	barcode: 300013,
+	firstname: 'Mila',
+	lastname: 'Najvar',
+	audiofile: '2520530.mp3',
+	photofile: 'Najvar_Mila.jpg',
+	altText: 'Mila Leanne Najvar, Computer and Information Sciences, I am grateful to the Citadel, my family and friends for helping me reach this milestone. This is by far one of my greatest accomplishments and I am proud to be a CGC graduate!',
+	filter1: 'College of Engineering',
+	filter2: 'Master'
+},
+{
+	barcode: 300011,
+	firstname: 'Galilea',
+	lastname: 'Medina',
+	audiofile: '2520807.mp3',
+	photofile: 'Medina_Galilea.jpg',
+	altText: 'Galilea Medina, Leadership, Heartfelt thanks to my family and friends for your encouragement and prayers. All of which helped make this day possible. May God\'s choice blessings be yours!',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Master'
+},
+{
+	barcode: 300007,
+	firstname: 'Lexus',
+	lastname: 'Jacobs',
+	audiofile: '2520814.mp3',
+	photofile: 'Jacobs_Lexus.jpg',
+	altText: 'Lexus Chardonnay Jacobs, Criminal Justice, You can only eat an elephant one bite at a time. Love you mom and dad!',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Master'
+},
+{
+	barcode: 300010,
+	firstname: 'Margaret',
+	lastname: 'McCown',
+	audiofile: '2520851.mp3',
+	photofile: 'McCown_Margaret.jpg',
+	altText: 'Margaret Nicole McCown, Biology, Magna Cum Laude, True heroism is remarkably sober, very undramatic. It is not the urge to surpass all others at whatever cost, but the urge to serve others at whatever cost.- Arthur Ashe',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Bachelor'
+},
+{
+	barcode: 300004,
+	firstname: 'Maggie',
+	lastname: 'Flynn',
+	audiofile: '2520942.mp3',
+	photofile: 'Flynn_Maggie.jpg',
+	altText: 'Maggie Nicole Flynn, Social Science, Magna Cum Laude, Sentenced to four years. Released early for good behavior.',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Bachelor'
+},
+{
+	barcode: 300003,
+	firstname: 'Kimberly',
+	lastname: 'Bertrand',
+	audiofile: '2521009.mp3',
+	photofile: 'Bertrand_Kimberly.jpg',
+	altText: 'Kimberly Bertrand, History & English,  Focus on what you must accomplish to get to where you want to be, but don\'t forget to live along the way.',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Master'
+},
+{
+	barcode: 300018,
+	firstname: 'Dani',
+	lastname: 'Willis',
+	audiofile: '2521033.mp3',
+	photofile: 'Willis_Dani.jpg',
+	altText: 'Dani Danee Willis, Literacy Education, Because of you, and those who were not able to be here, I feel that I am the luckiest man alive. Thank you. I love you.',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Doctoral'
+},
+{
+	barcode: 300012,
+	firstname: 'Mariska',
+	lastname: 'Mes',
+	audiofile: '2521038.mp3',
+	photofile: 'Mes_Mariska.jpg',
+	altText: 'Mariska Valerie Mes, Electrical Engineering, “Do what you can, with what you have, where you are.” —Theodore Roosevelt. Thank You Mom, Dad, Meaghan, and Grandpa. Love you all.',
+	filter1: 'College of Engineering',
+	filter2: 'Master'
+},
+{
+	barcode: 300014,
+	firstname: 'Lindsey',
+	lastname: 'Pantoja',
+	audiofile: '2521093.mp3',
+	photofile: 'Pantoja_Lindsey.jpg',
+	altText: 'Lindsey Marie Pantoja, Counselor Education, What a time to be alive.',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Doctoral'
+},
+{
+	barcode: 300001,
+	firstname: 'Shelby',
+	lastname: 'Amick',
+	audiofile: '2521096.mp3',
+	photofile: 'Amick_Shelby.jpg',
+	altText: 'Shelby Gwen Amick, Project Management, Cum Laude, If it were easy...EVERYONE would do it.',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Bachelor'
+},
+{
+	barcode: 300016,
+	firstname: 'April',
+	lastname: 'Simpson',
+	audiofile: '2521130.mp3',
+	photofile: 'Simpson_April.jpg',
+	altText: 'April Lynne Simpson, Business Administration, Summa Cum Laude',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Bachelor'
+},
+{
+	barcode: 300006,
+	firstname: 'Salma',
+	lastname: 'Garcia',
+	audiofile: '2521152.mp3',
+	photofile: 'Garcia_Salma.jpg',
+	altText: 'Salma Gisela Garcia, Sport Management, Magna Cum Laude, "I am the master of my fate...I am the captain of my soul." - Invictus',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Bachelor'
+},
+{
+	barcode: 300005,
+	firstname: 'Laura',
+	lastname: 'Garcia',
+	audiofile: '2521175.mp3',
+	photofile: 'Garcia_Laura.jpg',
+	altText: 'Laura Nicole Garcia, Civil Engineering, Thanks Mom, Dad, Cassi, Grant, Aubree and your family, Papaw, Mamaw, Nana, Darci, Karl, Tom and many more! Love you all!',
+	filter1: 'College of Engineering',
+	filter2: 'Doctoral'
+},
+{
+	barcode: 300020,
+	firstname: 'Diera',
+	lastname: 'Wright',
+	audiofile: '2521239.mp3',
+	photofile: 'Wright_Diera.jpg',
+	altText: 'Diera Denee Wright, English, I would like to give a heartfelt thanks to my family and friends who have supported me through this journey.',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Doctoral'
+},
+{
+	barcode: 300009,
+	firstname: 'Julie',
+	lastname: 'Langley',
+	audiofile: '2521273.mp3',
+	photofile: 'Langley_Julie.jpg',
+	altText: 'Julie Anne Langley, Political Science, I owe this achievement to my loving wife, Kelley Simpson. Her love is the true cause of my success.',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Master'
+},
+{
+	barcode: 300008,
+	firstname: 'Justin',
+	lastname: 'Kirk',
+	audiofile: '2525024.mp3',
+	photofile: 'Kirk_Justin.jpg',
+	altText: 'Justin W. Kirk, Business Administration & Project Management',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Master'
+},
+{
+	barcode: 300019,
+	firstname: 'Pui Shan',
+	lastname: 'Wong',
+	audiofile: '2525228.mp3',
+	photofile: 'Wong_Pui_Shan.jpg',
+	altText: 'Pui Shan Wong, Mechanical Engineering',
+	filter1: 'College of Engineering',
+	filter2: 'Master'
+},
+{
+	barcode: 300015,
+	firstname: 'M\'Randa',
+	lastname: 'Seefeldt',
+	audiofile: '2566909.mp3',
+	photofile: 'Seefeldt_M\'Randa.jpg',
+	altText: 'M\'Randa Seefeldt, Psychology, , Momma I made it!',
+	filter1: 'College of Arts and Sciences',
+	filter2: 'Master'
+}
+];
---
+++ settings.js
@@ -0,0 +1,44 @@
+var vgrad = vgrad || {};
+
+vgrad.Settings = {
+    Logofile: "images/schoollogo.png",
+    LogofileAltText: "The Citadel",
+    PrimaryColor: "#c9cacb",
+    SecondaryColor: "#ca072d",
+	Welcome: "Welcome to The Citadel's 2020 Virtual Graduation Ceremony",
+    Message: `Dear Graduate,<br><br>
+    <p>Lorem ipsum dolor sit amet, mundi eruditi repudiare has an. Bonorum convenire honestatis nam ex, ne has essent accusam instructior. Brute rationibus argumentum te qui. Te nam tritani pertinax delicata, ipsum impedit adipisci id has, ex graecis suavitate est. Dico soleat intellegat et sed, eu diam dicat denique his.</p>
+    <p>Modo prodesset accommodare est cu, ius in graeci euismod, at pertinax tractatos sententiae usu. No est novum admodum officiis, has in enim aliquip appetere. Error eligendi vis ea. Est id mollis singulis voluptatum, nam etiam placerat cu, sit ex ornatus admodum voluptatum. Stet fabellas theophrastus eam et, quaerendum cotidieque mel ad. Ad mei nulla corpora.</p>
+    <p>Your Commencement Team</p>`,
+    MessageTitle: "A Message from the Chancellor",
+    Videos: [
+        {
+            name: "Stacy Perasall",
+            title: "Commencement Speaker",
+            id: "u1wVNrkwjn8"
+        },
+        {
+            name: "Professor Daniel Pi",
+            title: "Faculty Member",
+            id: "wI8RS3kAMqY"
+        },
+        {
+            name: "Dean James Ryan",
+            title: "Graduate School of Education",
+            id: "bW0NguMGIbE"
+        },
+        {
+            name: "Chase Dahl",
+            title: "Graduate",
+            id: "DRiV4KZBoIY"
+        },
+        {
+            name: "Fernando Reimers",
+            title: "Faculty Member",
+            id: "_UbzM4lL2I4"
+        }
+    ],
+    SocialTitle: "Social Feed",
+    ClassTitle: "Class of 2020",
+}
+
